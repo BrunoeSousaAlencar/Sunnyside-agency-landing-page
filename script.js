e74f7b3d908c8915a993bbd829c3781ba47483d1
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-09-01 15:38:31 -0300

    estilos para mobile

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function mudouTamanho(){
+    const mudouTamanho = document.querySelector('.popup');
+    if(window.innerWidth >= 768){
+        mudouTamanho.style.display = 'none';
+    } 
+}
+
 function clickMenu(){
     const clickMenu = document.querySelector('.popup');
     if(clickMenu.style.display === "none"){
